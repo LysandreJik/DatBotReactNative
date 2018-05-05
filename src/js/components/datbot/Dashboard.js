658daf156914c95c2023b408bc456be9bc5d125d
Lysandre DEBUT <lysandre.debut@reseau.eseo.fr>
2018-05-05 18:15:06 +0200

    Completed the dashboard main screen.

--- src/js/components/datbot/Dashboard.js
+++ src/js/components/datbot/Dashboard.js
@@ -1,25 +1,43 @@
 import React from 'react';
 import { Text, View, Animated, Easing, ActivityIndicator, TouchableOpacity } from 'react-native';
+import { Font } from 'expo';
 import {DatBot} from "./DatBot";
 
 export default class Dashboard extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {fontLoaded: false};
+    }
 
+    async componentDidMount(){
+        await Font.loadAsync({
+            "Uni": require('../../../../assets/fonts/Uni.otf')
+        });
 
+        console.log("Font loaded ! ");
+        this.setState({fontLoaded: true});
+    }
 
     render(){
-        return(
-            <View style={styles.mainContainer}>
-                {DatBot.getController().getNonBannedBots().map(function(bot, key){
-                    return (
-                        <TouchableOpacity style={styles.botContainer}>
-                            <Text key={key} style={styles.text}>
-                                {bot.getName()}
-                            </Text>
-                        </TouchableOpacity>
-                    );
-                })}
-            </View>
-        );
+        if(this.state.fontLoaded){
+            return(
+                <View style={styles.mainContainer}>
+                    {DatBot.getController().getNonBannedBots().map(function(bot, key){
+                        return (
+                            <TouchableOpacity style={styles.botContainer}>
+                                <View style={styles.innerContainer}>
+                                    <Text key={key} style={{...styles.text, fontFamily:"Uni"}}>
+                                        {bot.getName()}
+                                    </Text>
+                                </View>
+                            </TouchableOpacity>
+                        );
+                    })}
+                </View>
+            );
+        }else{
+            return <View style={styles.mainContainer}><Text>Loading ...</Text></View>;
+        }
     }
 }
 
@@ -33,12 +51,20 @@ const styles = {
     botContainer:{
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'center'
+    },
+
+    innerContainer:{
         backgroundColor: "rgba(255, 89, 100, .4)",
-        height: "90%"
+        width:"90%",
+        height: "70%",
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: "20%"
     },
 
     text: {
-        color:"#FDD"
+        color:"#FDD",
+        fontSize: 30
     }
 };
